1dc0d046951a369b41ee26c777bab09783dc25ca
apeckham <apeckham@gmail.com>
2015-07-23 05:40:52 -0700

    add title and readme links

--- export.rb
+++ export.rb
@@ -5,12 +5,12 @@ require './shopify_extensions'
 ShopifyAPI::Base.site = ENV['SHOPIFY_BASE_SITE']
 
 CSV do |csv|
-  csv << %w'order_id tracking_number'
+  csv << %w'order_id tracking_number title'
 
   ShopifyAPI::Order.find_all(status: :any) do |order|
     order.fulfillments.each do |fulfillment|
       fulfillment.tracking_numbers.each do |tracking_number|
-        csv << [order.order_number, tracking_number]
+        csv << [order.order_number, tracking_number, order.order_number]
       end
     end
   end
